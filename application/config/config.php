1d32786892db5c70ba4e00231688b6a8f420116c
Kartik <kartikchauhan100@gmail.com>
2019-07-23 08:23:40 +0530

    codignator update

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/paidProjects/helloTech/';
+$config['base_url'] = 'http://localhost/helloTech/';
 
 /*
 |--------------------------------------------------------------------------
